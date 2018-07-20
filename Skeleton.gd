00094f5aa10f4dfde2519273e75035acbe638f33
fingerson <henfinzim@gmail.com>
2018-07-20 01:41:01 -0300

    better strings

--- Skeleton.gd
+++ Skeleton.gd
@@ -6,21 +6,37 @@ extends Node2D
 export(String) var anim = "Idle"
 
 var status = "play"
+var timer = 0
 
 func set_anim(animation):
+	anim = animation
 	if $Skeleton.current_animation != animation and status == "play":
 		$Skeleton.current_animation = animation
 		
 func play():
 	if status != "play":
-		$Skeleton.play()
+		if $Skeleton.current_animation != anim:
+			timer = 0
+		$Skeleton.seek(timer, true)
+		$Skeleton.play(anim)
+		print(timer)
+		$Skeleton.seek(timer, true)
 		status = "play"
 	pass
 func stop():
 	if status != "stop":
+		timer = $Skeleton.current_animation_position
+		print(timer)
 		$Skeleton.stop()
 		status = "stop"
 	pass
+
+func toggle():
+	if status == "stop":
+		play()
+	else:
+		stop()
+
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
